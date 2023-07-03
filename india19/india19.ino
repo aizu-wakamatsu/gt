a660add3e51f4e04d08ea3dd9643e36a280ed8e5
tari <38212728+aizu-wakamatsu@users.noreply.github.com>
2023-07-03 14:59:07 +0900

    add india29 & india22

---
+++ india19/india19.ino
@@ -0,0 +1,30 @@
+/*
+  india19.ino
+  test for filter
+*/
+
+#include "config_india19.h"
+
+float val_raw[SIZE_WINDOWS] = { 0 };
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  queueue();
+  val_raw[0] = (float)analogRead(PIN_ANLG);
+
+  delay(1000 / RATE_SAMPLE);
+}
+
+void queueue() {
+  // AULD 9 ... 0 LAST
+  short c = 0;
+  for (c = SIZE_WINDOWS - 1; c >= 1; --c) {
+    val_raw[c] = val_raw[c - 1];
+  }
+  val_raw[0] = 0;
+}
