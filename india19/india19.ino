b7f71075e105310aae0653fc5cc23547a742b6a1
tari <38212728+aizu-wakamatsu@users.noreply.github.com>
2023-07-04 13:54:30 +0900

    Update india19.ino

--- india19/india19.ino
+++ india19/india19.ino
@@ -5,7 +5,10 @@
 
 #include "config_india19.h"
 
-float val_raw[SIZE_WINDOWS] = { 0 };
+float x[SIZE_WINDOWS] = { 0 };
+float y[SIZE_WINDOWS] = { 0 };
+float coef[SIZE_WINDOWS] = { 0 };
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,7 +19,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   float sum;
   queueue();
-  val_raw[0] = (float)analogRead(PIN_ANLG);
+  x[0] = (float)analogRead(PIN_ANLG);
 
   delay(1000 / RATE_SAMPLE);
 }
@@ -25,14 +28,16 @@ void queueue() {
   // AULD 9 ... 0 LAST
   short c = 0;
   for (c = SIZE_WINDOWS - 1; c >= 1; --c) {
-    val_raw[c] = val_raw[c - 1];
+    x[c] = x[c - 1];
+    y[c] = y[c - 1];
   }
-  val_raw[0] = 0;
+  x[0] = 0;
+  y[0] = 0;
 }
 
 void fir() {
   short i = 0;
-  for (i = 0 ; i < SIZE_WINDOWS ; ++i){
-    
+  for (i = 0; i < SIZE_WINDOWS; ++i) {
+    y[0] += coef[i] * x[i];
   }
 }
